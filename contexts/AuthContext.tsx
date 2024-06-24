5343aff6ee25af3d33d9e5f09c5cab302d4cc7e9
andirakha <andi17rakha@gmail.com>
2024-06-24 17:46:10 +0700

    null

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
-// AuthContext.tsx
 import { checkIdCart, loginUser } from '@/lib/actions';
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 // Create a new context
 const AuthContext = createContext<{
@@ -13,35 +13,34 @@ const AuthContext = createContext<{
 });
 
 type AuthProviderProps = {
-    children: ReactNode; // Menggunakan ReactNode untuk menentukan bahwa children bisa berupa apa saja yang valid dalam JSX
-  };
+  children: ReactNode; // Menggunakan ReactNode untuk menentukan bahwa children bisa berupa apa saja yang valid dalam JSX
+};
 
 // Export provider component
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
-    // Check localStorage for login status on initial load
-    const storedLoggedIn = localStorage.getItem('isLoggedIn');
+    // Check cookies for login status on initial load
+    const storedLoggedIn = Cookies.get('isLoggedIn');
     return storedLoggedIn ? JSON.parse(storedLoggedIn) : false;
   });
   const router = useRouter();
 
-  // Update localStorage when isLoggedIn changes
+  // Update cookies when isLoggedIn changes
   useEffect(() => {
-    localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
+    Cookies.set('isLoggedIn', JSON.stringify(isLoggedIn));
   }, [isLoggedIn]);
 
   const login = async (username: string, password: string) => {
     try {
-        // Perform login logic (e.g., API call)
-        // Assuming loginUser and checkIdCart are imported properly
-        await loginUser(username, password);
-        await checkIdCart(username);
-        setIsLoggedIn(true);
-        router.push('/');
-        
-      } catch (error) {
-        throw new Error('Login failed. Please check your credentials.');
-      }
+      // Perform login logic (e.g., API call)
+      // Assuming loginUser and checkIdCart are imported properly
+      await loginUser(username, password);
+      await checkIdCart(username);
+      setIsLoggedIn(true);
+      router.push('/');
+    } catch (error) {
+      throw new Error('Login failed. Please check your credentials.');
+    }
   };
 
   return (
